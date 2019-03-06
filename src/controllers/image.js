ed407977307ed9c42aeeafb5c26e49637b4f18ce
isaac cobos <isaac-7@hotmail.com>
2019-03-06 17:46:46 +0100

    Funcionalidad de most popular y lastes Comments terminado

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -5,11 +5,13 @@ const md5 = require('md5');
 
 
 const { Image, Comment } = require('../models');
+const sidebar = require('../helpers/sidebar')
 
 const ctrl = {};
 
 ctrl.index = async (req, res) =>  {
-   const viewModel = { image:{}, comments:{}};
+   let viewModel = { image:{}, comments:{}};
+
    const image = await Image.findOne({filename:{$regex: req.params.image_id}});
    if(image) {
         image.views = image.views + 1;
@@ -17,6 +19,7 @@ ctrl.index = async (req, res) =>  {
         await image.save();
         const comments = await Comment.find({image_id: image._id});
         viewModel.comments = comments;
+        viewModel = await sidebar(viewModel);
         res.render('image', viewModel);
    }else {
        res.redirect('/');
